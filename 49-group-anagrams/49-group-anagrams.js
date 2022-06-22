4d3d62b082dc194f961cfa702216259b129b1fd1
vandana-26 <vandu.gupta26@gmail.com>
2022-06-22 15:47:45 +0530

    Time: 156 ms (77.85%), Space: 53.7 MB (48.82%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -2,23 +2,48 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+
+//1st way
+
+// var groupAnagrams = function(strs) {
+    
+//     let tempMap = new Map();
+//     for(let i = 0; i < strs.length; i++){
+//          let tempStr = strs[i].split('').sort().join('');
+//         if(!tempMap.has(tempStr)){
+//             tempMap.set(tempStr,[strs[i]])
+//         }
+//         else{
+//             let arr = tempMap.get(tempStr);
+//             arr.push(strs[i]);
+//             console.log(arr);
+//             tempMap.set(tempStr,arr);
+//         }
+//     }
+    
+//     console.log(tempMap);
+//     return Array.from(tempMap.values());
+    
+// };
+
+
+
+//2nd way
 var groupAnagrams = function(strs) {
     
-    let tempMap = new Map();
+    let tempMap = {};
+    
     for(let i = 0; i < strs.length; i++){
-         let tempStr = strs[i].split('').sort().join('');
-        if(!tempMap.has(tempStr)){
-            tempMap.set(tempStr,[strs[i]])
+         let key = strs[i].split('').sort().join('');
+        if(!tempMap[key]){
+            tempMap[key] = [strs[i]];
         }
         else{
-            let arr = tempMap.get(tempStr);
-            arr.push(strs[i]);
-            console.log(arr);
-            tempMap.set(tempStr,arr);
+          tempMap[key].push(strs[i]);
         }
     }
     
-    console.log(tempMap);
-    return Array.from(tempMap.values());
+  //  console.log(tempMap);
+    return Object.values(tempMap);
     
 };
