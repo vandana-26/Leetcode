cb168c43c9709f93a0af61b1d9ab4002656ec43a
vandana-26 <vandu.gupta26@gmail.com>
2022-08-19 13:42:10 +0530

    Time: 125 ms (5.66%), Space: 42.1 MB (48.11%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.js
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -13,19 +13,6 @@ var breakPalindrome = function(palindrome) {
     if(palindrome.length <= 1)
         return ""
     
-//     for(let i = 0; i < (palindrome.length)/2; i++){
-//         if(palindrome[i] == 'a')
-//             count++;
-//         else{
-//             newStr = palindrome.replaceAt(i,'a');
-//             return newStr;
-//         }
-//     }
-    
-//     if(count === (palindrome.length)/2)
-//        newStr = palindrome.replaceAt(palindrome.length-1,'b'); 
-//     return newStr;
-    
     let palindromeArr = palindrome.split('');
     
     for(let i = 0; i < Math.floor((palindromeArr.length)/2); i++){
