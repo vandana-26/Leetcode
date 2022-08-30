b64b2e5a14ebaf936e9ede32e54ba4e6119890d5
Vandana Gupta <vandu.gupta26@gmail.com>
2022-08-30 07:57:09 +0530

    Time: 100 ms (79.24%), Space: 49.9 MB (94.84%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+    let currentSum = nums[0], maxSum = nums[0];
+    
+    for(let i = 1; i < nums.length; i++){
+        currentSum = Math.max(nums[i],currentSum + nums[i]);
+        maxSum = Math.max(maxSum,currentSum);
+        
+    }
+    
+    return maxSum;
+    
+};
