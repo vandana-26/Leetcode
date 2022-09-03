a247d1ee60bfee8702120cc8418ed85644c06ff5
Vandana Gupta <vandu.gupta26@gmail.com>
2022-09-03 17:52:37 +0530

    Time: 113 ms (26.12%), Space: 42.7 MB (72.47%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -3,22 +3,56 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+//FIrst Way to solve
+// var intersect = function(nums1, nums2) {
+//         let tempMap = new Map();
+//     let arr = [];
+    
+//     for(let i = 0; i < nums1.length; i++){
+//         if(!tempMap.has(nums1[i]))
+//             tempMap.set(nums1[i],1);
+//         else
+//             tempMap.set(nums1[i], tempMap.get(nums1[i])+1)
+//     }
+    
+//     for(let i = 0; i < nums2.length; i++){
+//         if(tempMap.has(nums2[i]) && tempMap.get(nums2[i]) > 0){
+//           arr.push(nums2[i]);
+//             tempMap.set(nums2[i],tempMap.get(nums2[i])-1);
+//         }  
+//     }
+//     return arr;
+// };
+
+
+//Second APproach
+
 var intersect = function(nums1, nums2) {
-        let tempMap = new Map();
-    let arr = [];
     
-    for(let i = 0; i < nums1.length; i++){
-        if(!tempMap.has(nums1[i]))
-            tempMap.set(nums1[i],1);
-        else
-            tempMap.set(nums1[i], tempMap.get(nums1[i])+1)
+    let i = 0, j = 0, k=0;
+    
+    nums1.sort((a,b) =>{
+        return a-b;
+    })
+    
+    nums2.sort((a,b) =>{
+        return a-b;
+    })
+    
+    while(i < nums1.length && j < nums2.length){
+        if(nums1[i] < nums2[j]){
+            i++;
+        }
+        else if(nums1[i] > nums2[j])
+            j++;
+        else{
+            nums1[k] = nums1[i];
+            i++;
+            j++;
+            k++;
+        }
     }
     
-    for(let i = 0; i < nums2.length; i++){
-        if(tempMap.has(nums2[i]) && tempMap.get(nums2[i]) > 0){
-          arr.push(nums2[i]);
-            tempMap.set(nums2[i],tempMap.get(nums2[i])-1);
-        }  
-    }
-    return arr;
-};
+    return nums1.slice(0,k);
+}
