8a2638ec69570dba1d73d84ad709814a78208ee4
Vandana Gupta <vandu.gupta26@gmail.com>
2022-09-13 05:31:15 +0530

    Time: 87 ms (56.49%), Space: 41.9 MB (86.46%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -3,6 +3,8 @@
  * @param {number} target
  * @return {boolean}
  */
+
+/* First Method
 var searchMatrix = function(matrix, target) {
     for(let i = 0; i < matrix.length; i++){
        let j = matrix[i].length - 1;
@@ -23,6 +25,33 @@ var searchMatrix = function(matrix, target) {
 
             }
           }
+    }
+    return false;
+};
+*/
+
+//Second method using Binary Search
+var searchMatrix = function(matrix, target) {
+ let rows = matrix.length;
+ let cols = matrix[0].length;
+    
+    if(rows == 0)
+        return false;
+    
+    let left = 0, right = rows*cols -1;
+     let midIndex, midEle;
+    while(left <= right){
+        midIndex = Math.floor((left+right)/2);
+        midEle = matrix[Math.floor(midIndex/cols)][midIndex%cols];
+        
+        if(midEle == target)
+            return true;
+        
+        else if(midEle > target)
+            right = midIndex- 1;
+        else
+            left = midIndex + 1
+        
     }
     return false;
 };
