9bfd89cf83c216da1b92043e2225d878e1462a22
Vandana Gupta <vandu.gupta26@gmail.com>
2024-12-14 09:37:32 -0600

    Time: 22 ms (17.84%), Space: 66.3 MB (26.92%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let prefix = new Array(nums.length).fill(1);
+    let suffix = new Array(nums.length).fill(1);
+    let output = [];
+
+    for(let i = 1; i < nums.length; i++) {
+     prefix[i] = prefix[i-1] * nums[i-1];
+    }
+    for(let i = nums.length - 2; i >= 0; i--) {
+     suffix[i] = suffix[i+1] * nums[i+1];
+    }
+
+    for(let i = 0; i < nums.length; i++) {
+     output[i] = prefix[i] * suffix[i];
+    }
+ 
+ return output;
+};
